119672ee2869c8c090e5ffa0f3c6ca43dd803c7d
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2023-09-05 19:20:42 +0800

    Add files via upload

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '2174d146bb9c0eab47529b2e77d6b526',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
